4bc9905a2a929568e69d0ed5c7fbfad41d5af906
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-09-24 21:36:47 +0530

    Added routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import SignIn from './auth/sign-in/index.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './home'
+import Dashboard from './dashboard'
 
+const root = createBrowserRouter(
+   [
+    {
+        element: <App />,
+        children : [
+            {
+                path: '/',
+                element: <Home />
+            },
+             {
+                path: '/dashboard',
+                element: <Dashboard />
+            }
+        ]
+            
+    },
+    {
+        path: '/auth/sign-in',
+        element: <SignIn />
+    },
+   ]
+)
 createRoot(document.getElementById('root')).render(
-    <App />
+    <RouterProvider router={root}>
+        <App />
+    </RouterProvider>
 )

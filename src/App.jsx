123cd45ec5e356a9c85a9b6cffc7f97c2f73c30f
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-09-24 20:58:44 +0530

    Setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <div>App</div>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 

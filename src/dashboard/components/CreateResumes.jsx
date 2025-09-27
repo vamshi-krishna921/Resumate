de6e092cefddb38b4335c8cdb33d3c8b6a3b5017
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-09-27 15:08:20 +0530

    Added templates

--- src/dashboard/components/CreateResumes.jsx
+++ src/dashboard/components/CreateResumes.jsx
@@ -21,7 +21,13 @@ function CreateResumes() {
   const [loading, setLoading] = useState(false);
   const { user } = useUser();
   const navigate = useNavigate();
-
+  const templates = [
+    { id: 0, name: "Modern" },
+    { id: 1, name: "Minimalist" },
+    { id: 2, name: "Creative" },
+  ];
+  const [selectedTemplate, setSelectedTemplate] = useState(templates[0].id);
+  console.log("Selected Template ID:", selectedTemplate);
   //* Function to handle creating a new resume
   const handleCreateResume = () => {
     setLoading(true);
@@ -31,15 +37,17 @@ function CreateResumes() {
       title: resumeTitle,
       userEmail: user?.primaryEmailAddress?.emailAddress,
       userName: user?.fullName,
+      templateId: selectedTemplate,
     };
 
     createResume(data)
       .then((res) => {
         console.log("Resume created successfully:", res.data);
         setResumeTitle("");
+        setSelectedTemplate(templates[0].id);
         setOpenDialogBox(false);
         setLoading(false);
-        navigate(`/dashboard/resume/`+resume.resumeId+"/edit");
+        navigate(`/dashboard/resume/${id}/edit`);
       })
       .catch((err) => {
         console.error("Error creating resume:", err);
@@ -68,6 +76,24 @@ function CreateResumes() {
                 value={resumeTitle}
                 onChange={(e) => setResumeTitle(e.target.value)}
               />
+              <div className="mt-4">
+                <div className="text-lg font-semibold font-body mb-2">
+                  Select template
+                </div>
+                <div className="flex gap-3 flex-wrap focus:border-blue-600">
+                  {templates.map((template) => (
+                    <Button
+                      key={template.id}
+                      variant={
+                        selectedTemplate === template.id ? "default" : "outline"
+                      }
+                      onClick={() => setSelectedTemplate(template.id)}
+                    >
+                      {template.name}
+                    </Button>
+                  ))}
+                </div>
+              </div>
             </DialogDescription>
           </DialogHeader>
           <div className="flex justify-end mt-4">
@@ -80,10 +106,10 @@ function CreateResumes() {
             </Button>
             <Button
               variant="destructive"
-              disabled={resumeTitle.trim() === ""||loading}
+              disabled={resumeTitle.trim() === "" || loading}
               onClick={handleCreateResume}
             >
-              {loading ? <LoaderCircle className="animate-spin"/> : "Confirm"}
+              {loading ? <LoaderCircle className="animate-spin" /> : "Confirm"}
             </Button>
           </div>
         </DialogContent>

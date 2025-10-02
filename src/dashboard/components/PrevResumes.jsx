3c6707aeac04dc87e7ca14ddcfc9fa322bfcbe7d
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-02 13:25:19 +0530

    Added Menu

--- src/dashboard/components/PrevResumes.jsx
+++ src/dashboard/components/PrevResumes.jsx
@@ -1,22 +1,50 @@
 import React from "react";
 import resumeIcon from "../../assets/resumeIcon.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-function PrevResumes({ resume, index }) {
+function PrevResumes({ resume }) {
+  const navigate = useNavigate();
   return (
-    <>
-      <Link to={`/dashboard/resume/`+resume.documentId+"/edit"}>
-        <div
-          key={index}
-          className="hover:scale-103 rounded-lg w-[288px] h-[400px] flex gap-3.5 flex-col items-center justify-start cursor-pointer transform-transition duration-200"
-        >
+    <div className="relative w-[288px] h-[400px]">
+      <Link to={`/dashboard/resume/${resume.documentId}/edit`}>
+        <div className="hover:scale-103 rounded-lg w-full h-full flex flex-col gap-3.5 items-center justify-start cursor-pointer transition-transform duration-200">
           <div className="w-[98%] h-[90%] bg-color rounded-lg flex items-center justify-center">
             <img src={resumeIcon} alt="ResumeIcon" />
           </div>
           <h1 className="text-lg font-heading font-bold">{resume.title}</h1>
         </div>
       </Link>
-    </>
+
+      {/* Dropdown in bottom-left corner */}
+      <div className="absolute top-2 right-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <button className="p-2 rounded bg-gray-100  transition-transform duration-150 ease-in hover:scale-109 cursor-pointer text-3xl ">
+              ⋮
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem
+              onClick={() =>
+                navigate(`/dashboard/resume/${resume.documentId}/edit`)
+              }
+            >
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem  onClick={() =>
+                navigate(`/my-resume/${resume.documentId}/view`)
+              }>Download</DropdownMenuItem>
+            <DropdownMenuItem>Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
   );
 }
 
